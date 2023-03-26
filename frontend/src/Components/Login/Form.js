25e2c7dd33c5257e7f58a03e067c4c051f6fde17
Ayush Singla <ayushsingla2703@gmail.com>
2023-03-27 03:05:28 +0530

    css Fixed

--- frontend/src/Components/Login/Form.js
+++ frontend/src/Components/Login/Form.js
@@ -187,14 +187,16 @@ function Form(props) {
               </div>
             )}
             {!props.isRegistered && (
-              <Button classname ={styles1.button} text = " Register" onClick = {handleRegister}/>
+              <div style ={{position:"relative",top:"60px" }}>
+              <Button classname ={styles1.button} text = " Register" onClick = {handleRegister} />
+              </div>
             )}
             
           {/* </div> */}
           <div>
               {
                 (err1 && !props.isRegistered) &&
-                <div style = {{color : "red", margin:"0px" }}>
+                <div style = {{color : "red", margin:"0px",position:"relative" ,top:"60px"}}>
                   ! Password not matching
                 </div>
               }
